8cd51b2873ddd6be7f5c474e053f4f0863371783
obrad97 <janiobrad@gmail.com>
2021-09-26 01:33:09 +0200

    input onchange update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,33 @@
 const btn = document.getElementById("btn");
+let email = document.getElementById('email');
+
+email.onchange = () => {
+    let errorMsg = document.getElementsByClassName('error-msg')[0];
+    if(errorMsg) { 
+        errorMsg.remove();
+    }
+
+    if (email.value != '') {
+        let lastAtPos = email.value.lastIndexOf('@');
+        let lastDotPos = email.value.lastIndexOf('.');
+        if (!(lastAtPos < lastDotPos && lastAtPos > 0 && email.value.indexOf('@@') === -1 && lastDotPos > 2 && (email.value.length - lastDotPos) > 2)) {
+            const errorMsg = document.createElement('p');
+            errorMsg.className = 'error-msg';
+            const text = document.createTextNode('Oops! Please check your email');
+            errorMsg.appendChild(text); 
+            const form = document.getElementById('form');
+            form.appendChild(errorMsg);
+        }
+    } else {
+        const errorMsg = document.createElement('p');
+        errorMsg.className = 'error-msg';
+        const text = document.createTextNode('Please enter an email address');
+        errorMsg.appendChild(text); 
+        const form = document.getElementById('form');
+        form.appendChild(errorMsg);
+    }
+
+} 
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
